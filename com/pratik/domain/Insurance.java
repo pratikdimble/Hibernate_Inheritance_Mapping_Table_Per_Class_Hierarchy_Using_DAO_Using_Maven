0f3161341d3d6c3b7d4062f51c2eeb26383b3710
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-13 06:19:46 +0530

    Parent or Master entity/Domain class

---
+++ com/pratik/domain/Insurance.java
@@ -0,0 +1,31 @@
+package com.pratik.domain;
+
+public class Insurance {
+	private int id;
+	private String name;
+	private String company;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	@Override
+	public String toString() {
+		return "Insurance [id=" + id + ", name=" + name + ", company=" + company + "]";
+	}
+	
+	
+}
