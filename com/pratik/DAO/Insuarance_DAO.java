8f6d27304e0eca6e3732691f94f37b19f8a67060
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-13 06:22:07 +0530

    Interface class

---
+++ com/pratik/DAO/Insuarance_DAO.java
@@ -0,0 +1,11 @@
+package com.pratik.DAO;
+
+import java.util.List;
+
+import com.pratik.domain.Insurance;
+
+public interface Insuarance_DAO {
+	public String savedata() ;
+	public List<Insurance> getdata();
+
+}
